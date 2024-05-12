db7219a3c2528e70a3e85e9b07ef2fb0f5e67c72
YBurgosLucas <burgos.13.yamel@gmail.com>
2024-05-11 23:25:51 -0300

    modificaciones

--- tp3/Banco.php
+++ tp3/Banco.php
@@ -1,4 +1,8 @@
 <?php
+    include_once "CuentaCorriente.php";
+    include_once "CajaAhorro.php";
+    include_once "Cuenta.php";
+
     class Banco{
         private $coleCCorriente;
         private $coleCAhorro;
@@ -6,10 +10,128 @@
         private $coleCliente;
 
         public function __construct($coleCCorriente, $coleCAhorro, $ultValorCuentaAsignado, $coleCliente){
+            
             $this->coleCCorriente=$coleCCorriente;
             $this->coleCAhorro=$coleCAhorro;
             $this->ultValorCuentaAsignado=$ultValorCuentaAsignado;
             $this->coleCliente=$coleCliente;
         }
+        public function getColCCorriente(){
+            return $this->coleCCorriente;
+        }
+        public function getColCAhorro(){
+            return $this->coleCAhorro;
+        }
+        public function getUltValorCuenta(){
+            return $this->ultValorCuentaAsignado;
+        }
+        public function getColeCliente(){
+            return $this->coleCliente;
+        }
+        public function setColCCorriente($coleCCorriente){
+            $this->coleCCorriente=$coleCCorriente;
+        }
+        public function setColCAhorro($coleCAhorro){
+            $this->coleCAhorro=$coleCAhorro;
+        }
+        public function setUltValorCuenta($ultValorCuentaAsignado){
+            $this->ultValorCuentaAsignado=$ultValorCuentaAsignado;
+        }
+        public function setColeCliente($coleCliente){
+            $this->coleCliente=$coleCliente;
+        }
 
+        public function retornarCCorriente(){
+            $cad="";
+            foreach($this->getColCCorriente() as $cCorriente){
+                $cad.=$cCorriente."\n";
+            }
+            return $cad;
+        }
+        public function retornarCAhorro(){
+            $cad="";
+            foreach($this->getColCAhorro() as $cAhoro){
+                $cad.=$cAhorro."\n";
+            }
+            return $cad;
+        }
+
+        public function retornarCCliente(){
+            $cad="";
+            foreach($this->getColeCliente() as $cliente){
+                $cad.=$cliente."\n";
+            }
+            return $cad;
+        }
+        public function __toString(){
+            $cad="\nColeccion C.Corriente:\n".$this->retornarCCorriente().
+                 "\nColeccion C.Ahorro:\n".$this->retornarCAhorro().
+                 "\nUltimo valor asignado:".$this->getUltValorCuenta().
+                 "\nColeccion Clientes:\n".$this->retornarCCliente();
+            return $cad;
+        }
+
+        public function incorporarCliente($objCliente){
+            $colCliente=$this->getColeCliente();
+            $incorporado=null;
+            foreach($colCliente as $cliente){
+                if($cliente->getDni() == $objCliente->getDni()){
+                    $incorporado=false;
+                }
+            }
+            if($incorporado==null){
+                $i=count($colCliente);
+                $colCliente[$i]=$objCliente;
+                $this->setColeCliente($colCliente);
+                $incorporado=true;
+            }
+            return $incorporado;
+        }
+
+        public function incorporarCuentaCorriente($numCliente,$montoDescubierto){
+            $colCliente=$this->getColeCliente();
+            $colCCorriente=$this->getColCCorriente();
+            $clienteE=false;
+            $i=0;
+            $realizado=false;
+            while($i<count($colCliente) && $clienteE==false){
+                if($colCliente[$i]->getNroCliente() == $numCliente ){
+                    $clienteE=true;
+                    $cliente=$colCliente[$i];
+                }
+                $i++;
+            }
+            if($clienteE){
+                $j=count($colCCorriente);
+                $objCCorriente=new CuentaCorriente(($j+1),$cliente,0,$montoDescubierto);
+                $colCCorriente[$j]=$objCCorriente;
+                $this->setColCCorriente($colCCorriente);
+                $realizado=true;
+            }
+            return $realizado;
+        }
+        public function realizarRetiro($numCuenta, $monto){
+            $i=0;
+            $j=0;
+            $coleCAhorro=$this->getColCAhorro();
+            $colCCorriente=$this->getColCCorriente();
+            $numEncontrado=false;
+            while($i<count($coleCAhorro) && $numEncontrado==false){
+                if($coleCAhorro[$i]->getNroCuenta()== $numCuenta){
+                    $numEncontrado=true;
+                    
+                    
+                }
+
+            }
+            if($numEncontrado ==false){
+                while($j<count($colCCorriente) && $numEncontrado== false){
+                    if($colCCorriente[$j]->getNroCuenta()== $numCuenta){
+                        $numEncontrado=true;
+                        $colCCorriente[$j]->realizarRetiro($monto);
+                    }
+
+                }
+            }
+        }
     }
--- tp3/test_Banco.php
+++ tp3/test_Banco.php
@@ -6,50 +6,43 @@
     include_once "Cuenta.php";//padre
     include_once "CajaAhorro.php";//hijo
     
-    $objCliente=new Cliente(1234, "juan", "gonzalez", 1);
-    echo "________________Caja Ahorro______________________\n";
-    $objCAhorro=new CajaAhorro(888, $objCliente, 200);
-    echo $objCAhorro."\n";
-    echo "________________deposito_________________________\n";
-    $resutaldo=$objCAhorro->realizarDeposito(1000);
-    if($resutaldo){
-        echo "deposito exitoso\n";
-        echo $objCAhorro."\n";
+    include "Banco.php";
+    $colCCorriente=[];
+    $colCAhorro=[];
+    $ultValor=0;
+    $colCliente=[];
+    $objBanco=new Banco($colCCorriente,$colCAhorro,$ultValor, $colCliente);
+    $objCliente1=new Cliente(1234, "juan", "gonzalez", 1);
+    $objCliente2=new Cliente(4321, "luis", "hernandez", 2);
+
+    $respuesta=$objBanco->incorporarCliente($objCliente1);
+    if($respuesta){
+        echo "ingresado\n";
     }
     else{
-        echo "deposito rechazado\n";
-        echo $objCAhorro."\n";
+        echo "no ingresado\n";
     }
-    echo "____________________RETIROCA________________________\n";
-    $retiro=$objCAhorro->realizarRetiro(301);
-    if($retiro){
-        echo "retiro realizado\n";
-        echo $objCAhorro."\n";
+    
+    $respuesta1=$objBanco->incorporarCliente($objCliente2);
+    if($respuesta1){
+        echo "ingresado\n";
     }
     else{
-        echo "retiro no realizado supera los limites\n";
-        echo $objCAhorro."\n";
+        echo "no ingresado\n";
     }
-    echo "________________Cuenta Corriente_________________\n";
-    $objCCorriente=new CuentaCorriente(777,$objCliente,100,200);
-    echo $objCCorriente."\n";
-    echo "____________________Deposito________________________\n";
-    $resutaldo=$objCCorriente->realizarDeposito(300);
-    if($resutaldo){
-        echo "deposito Realizado\n";
-        echo $objCCorriente."\n";
+
+    $respuesta=$objBanco->incorporarCuentaCorriente(1, 200);
+    if($respuesta){
+        echo "cuenta creada\n";
     }
     else{
-        echo "deposito no realizado\n";
-        echo $objCCorriente."\n";
+        echo "cliente no existe\n";
     }
-    echo "____________________RETIROCC________________________\n";
-    $retiro=$objCCorriente->realizarRetiro(301);
-    if($retiro){
-        echo "retiro realizado\n";
-        echo $objCCorriente."\n";
+    $respuesta2=$objBanco->incorporarCuentaCorriente(2, 200);
+    if($respuesta2){
+        echo "cuenta creada\n";
+        print_r($objBanco);
     }
     else{
-        echo "retiro no realizado supera los limites\n";
-        echo $objCCorriente."\n";
+        echo "cliente no existe\n";
     }
