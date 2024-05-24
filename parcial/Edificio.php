4c1d0b45783e3e2b8a8a96061ee35613f5b45613
YBurgosLucas <burgos.13.yamel@gmail.com>
2024-05-24 14:33:12 -0300

    nuevo

---
+++ parcial/Edificio.php
@@ -0,0 +1,93 @@
+<?php
+class Edificio{
+    private $direccion;
+    private $coleccionInmuebles;
+    private $objAdministrador;
+
+    public function __construct($direccion, $coleccionInmuebles, $objAdministrador){
+        $this->direccion=$direccion;
+        $this->coleccionInmuebles=$coleccionInmuebles;
+        $this->objAdministrador=$objAdministrador;
+    }
+    public function getDireccion(){
+        return $this->direccion;
+    }
+    public function getColeccionInmuebles(){
+        return $this->coleccionInmuebles;
+    }
+    public function getObjAdministrador(){
+        return $this->objAdministrador;
+    }
+    public function setDireccion($direccion){
+        $this->direccion=$direccion;
+    }
+    public function setColeccionInmuebles($coleccionInmuebles){
+        $this->coleccionInmuebles=$coleccionInmuebles;
+    }
+    public function setObjAdministrador($objAdministrador){
+        $this->objAdministrador=$objAdministrador;
+    }
+    public function retornarColeccion(){
+        $cad="";
+        foreach($this->getColeccionInmuebles() as $inmuebles){
+            $cad.=$inmuebles."\n";
+        }
+        return $cad;
+    }
+    public function __toString(){
+        $cadena="Direccion:".$this->getDireccion().
+                "\nColeccion inmuebles:\n".$this->retornarColeccion().
+                "\nCant. total  inmuebles:".count($this->getColeccionInmuebles()).
+                "\nAdministrador:\n".$this->getObjAdministrador();
+        return $cadena;
+    }
+
+    public function darInmueblesDisponibles($tipoUso, $costoMensualMax){
+        $colecInmDisponible=[];
+        foreach ($this->getColeccionInmuebles() as $unInmueble){
+            if($unInmueble->getObjInquilino() ==null ){
+                if(($unInmueble->getTipoUso()== $tipoUso)&& $unInmueble->getCostoMensual()<=$costoMensualMax){
+                    $i=count($colecInmDisponible);
+                    $colecInmDisponible[$i]=$unInmueble;
+                }
+            }
+        }
+        return $colecInmDisponible;
+    }
+    public function  buscarInmueble($objInmueble){
+        $resultado=-1;
+        foreach ($this->getColeccionInmuebles() as $indice => $unInmueble) {
+            if($unInmueble->getCodigo() == $objInmueble->getCodigo()){
+                $resultado=$indice;
+            }  
+        }
+        return $resultado;
+    }
+
+    public function registrarAlquilerInmueble($tipoUso, $costoMensualMax, $objPersona){//encontrar primer inmueble disponible y de ahi comparar si ese inmueble es el indicado que buscan los inquilinos
+        $encontrado=false;
+        $colInmuebles=$this->getColeccionInmuebles();
+        $i=0;
+        while ($i<count($colInmuebles) && $encontrado==FALSE){
+            if($colInmuebles[$i]->estaDisponible($tipoUso, $costoMensualMax)){
+                    $encontrado=true;
+                    $coleccionInmuebles[$i]->alquilar($objPersona);
+
+            }
+            $i++;
+        }
+        return $encontrado;
+     
+    
+    }
+
+    public function calculaCostoEdificio(){
+        $costo=0;
+        foreach($this->getColeccionInmuebles() as $unIm){
+            if($unIm->getObjInquilino()!= null){
+                $costo+=$unIm->getCostoMensual();
+            }
+        }
+        return $costo;
+    }
+}
